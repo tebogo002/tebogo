7a5f23c31ff469677e765f7ce3781682a0afc096
Tebogo <tebogogift719@gmail.com>
2026-08-24 17:08:52 +0200

    Delete ticket booker.py

--- ticket booker.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-name_of_band_or_artist = input("Enter the name of the band or artist: ")
-
-print(f"\nCongratulations, {name}! Your ticket to see {name_of_band_or_artist} has been booked successfully!")
